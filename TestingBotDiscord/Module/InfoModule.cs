66abd405c83d2606b038babbdb638c1da1496b16
Hamza Saddouki <hsaddouki@gmail.com>
2017-12-20 15:21:12 +0100

    Add project files.

---
+++ TestingBotDiscord/Module/InfoModule.cs
@@ -0,0 +1,34 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingBotDiscord.Module
+{
+    public class InfoModule: ModuleBase<SocketCommandContext>
+    {
+        [Command("say")]
+        [Summary("Echos a message.")]
+        public async Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
+        {
+            // ReplyAsync is a method on ModuleBase
+            await ReplyAsync(echo);
+        }
+
+        [Command("join", RunMode = RunMode.Async)]
+        public async Task JoinChannel(IVoiceChannel channel = null)
+        {
+            channel = channel ?? (Context.Message.Author as IGuildUser)?.VoiceChannel;
+            if (channel is null)
+            {
+                await Context.Channel.SendMessageAsync("User must be in a voice channel");
+                return;
+            }
+
+            var audioClient = await channel.ConnectAsync();
+        }
+    }
+}
